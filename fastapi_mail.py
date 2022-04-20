38ce1d8bc0dcd8158814089ee8e6bdfe392fbc54
nicky-LV <nickystnkvs@outlook.com>
2022-04-20 18:31:04 +0100

    Conditional for checking whether template exists within the templates dir

--- fastapi_mail.py
+++ fastapi_mail.py
@@ -1,5 +1,5 @@
 import smtplib
-
+import os
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from typing import List, Optional
@@ -22,11 +22,16 @@ class Mail:
         self.smtp_port = smtp_port
         self.tls = tls
 
-        # Load Jinja2 environment for discovering templates
-        self.env = Environment(
-            loader=PackageLoader("yourapp"),
-            autoescape=select_autoescape()
-        )
+        # Check if templates dir exists.
+        if os.path.exists("templates"):
+            # Load Jinja2 environment for discovering templates
+            self.env = Environment(
+                loader=PackageLoader("main"),
+                autoescape=select_autoescape()
+            )
+
+        else:
+            raise OSError("There must be a python module named templates in the same directory as main.py")
 
     def send_email_template(self, recipients: List[str], subject, email_path: str, context: dict):
         """
@@ -42,22 +47,26 @@ class Mail:
         # For example, html, text, images, etc.
         msg = MIMEMultipart()
 
-        # Search for template
-        html_template = self.env.get_template(email_path)
+        # Search for template.
+        if os.path.exists(f"./templates/{email_path}"):
+            html_template = self.env.get_template(email_path)
 
-        # Populate template with context
-        populated_html_template = html_template.render(**context)
+            # Populate template with context
+            populated_html_template = html_template.render(**context)
 
-        # Set subject and recipients
-        msg['Subject'] = subject
-        msg['From'] = self.username
-        msg['To'] = ",".join(recipients)
+            # Set subject and recipients
+            msg['Subject'] = subject
+            msg['From'] = self.username
+            msg['To'] = ",".join(recipients)
 
-        # Attach populated jinja2 template
-        msg.attach(MIMEText(populated_html_template))
+            # Attach populated jinja2 template to the body of the email
+            msg.attach(MIMEText(populated_html_template))
 
-        # Connect to SMTP server
-        server = smtplib.SMTP(host=self.smtp_host, port=self.smtp_port)
-        server.starttls()
-        server.login(user=self.username, password=self.password)
-        server.send_message(msg)
+            # Connect to SMTP server
+            server = smtplib.SMTP(host=self.smtp_host, port=self.smtp_port)
+            server.starttls()
+            server.login(user=self.username, password=self.password)
+            server.send_message(msg)
+
+        else:
+            raise FileNotFoundError(f"Template named {email_path} was not found.")
