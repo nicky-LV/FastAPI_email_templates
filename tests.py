98b0377b5e7c6b75fb9a15edb2910ee3ceb3f0dc
nicky-LV <nickystnkvs@outlook.com>
2022-04-20 18:31:15 +0100

    Test for template discovery

--- tests.py
+++ tests.py
@@ -1,5 +1,23 @@
 import os
+from jinja2 import Environment, PackageLoader, select_autoescape
 import smtplib
+
+
+def test_template_discovery():
+    """ Create a test template within the templates directory and load it. """
+    if not os.path.exists("./templates/test.html"):
+        os.mknod("./templates/test.html")
+
+    # Setup jinja2 environment
+    env = Environment(
+        loader=PackageLoader("main"),
+        autoescape=select_autoescape()
+    )
+
+    template = env.get_template("test.html")
+
+    if not template:
+        raise FileNotFoundError("Template could not be found.")
 
 
 def test_smtp_server_connection():
