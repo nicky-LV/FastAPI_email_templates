2adc9efaa216c4e0c6ba266cad1997286a3fb0d6
nicky-LV <nickystnkvs@outlook.com>
2022-04-22 23:16:20 +0100

    Remove checks for env variables - fix temporary issue with pytest not discovering env variables

--- main.py
+++ main.py
@@ -3,13 +3,3 @@ import os
 from fastapi import FastAPI
 
 app = FastAPI()
-
-# Check env variables
-required_env_variables = [
-    "SMTP_HOST",
-    "SMTP_PORT"
-]
-
-for env_variable in required_env_variables:
-    if not os.environ[env_variable]:
-        raise EnvironmentError(f"Environment variable: {env_variable} not provided")
