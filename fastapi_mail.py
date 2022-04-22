61c8b415726036c9153c3fa236dcca24d3d15700
nicky-LV <nickystnkvs@outlook.com>
2022-04-22 23:58:49 +0100

    tls -> ssl. tls is on by default. For ssl must send EHLO to smtp server.
    Added bcc / cc functionality to mail engine.

--- fastapi_mail.py
+++ fastapi_mail.py
@@ -14,13 +14,13 @@ class EmailModel(BaseModel):
 
 
 class Mail:
-    def __init__(self, username: str, password: str, smtp_host: str, smtp_port: int = 433, tls: bool = True):
+    def __init__(self, username: str, password: str, smtp_host: str, smtp_port: int = 587, ssl: bool = True):
         # SMTP details
         self.username = username
         self.password = password
         self.smtp_host = smtp_host
         self.smtp_port = smtp_port
-        self.tls = tls
+        self.ssl = ssl
 
         # Check if templates dir exists.
         if os.path.exists("templates"):
@@ -33,13 +33,16 @@ class Mail:
         else:
             raise OSError("There must be a python module named templates in the same directory as main.py")
 
-    def send_email_template(self, recipients: List[str], subject, email_path: str, context: dict):
+    def send_email_template(self, recipients: List[str], subject: str, email_path: str,
+                            context: dict = None, cc: List[str] = None, bcc: List[str] = None):
         """
         Sends email jinja2 template
         :param recipients: List[str] of recipient email addresses.
         :param subject: str - Subject of email.
         :param email_path: str - Path of email template (.html)
         :param context: dict - Context to pass into jinja2 template
+        :param cc: List[str] - Carbon copy recipients
+        :param bcc: List[str] - Blind carbon copy recipients
         :return:
         """
 
@@ -51,20 +54,32 @@ class Mail:
         if os.path.exists(f"./templates/{email_path}"):
             html_template = self.env.get_template(email_path)
 
-            # Populate template with context
-            populated_html_template = html_template.render(**context)
+            # Populate template with context, if available
+            if context:
+                html_template = html_template.render(**context)
 
             # Set subject and recipients
             msg['Subject'] = subject
             msg['From'] = self.username
             msg['To'] = ",".join(recipients)
 
+            if bcc:
+                msg['Bcc'] = bcc
+
+            if cc:
+                msg['Cc'] = cc
+
             # Attach populated jinja2 template to the body of the email
-            msg.attach(MIMEText(populated_html_template))
+            msg.attach(MIMEText(html_template))
 
             # Connect to SMTP server
             server = smtplib.SMTP(host=self.smtp_host, port=self.smtp_port)
+
+            if self.ssl:
+                server.ehlo()
+
             server.starttls()
+
             server.login(user=self.username, password=self.password)
             server.send_message(msg)
 
